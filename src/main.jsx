ed3a3f1334666671a74c183088250b6064e051df
Gowtham Nachu <gowthamnachu545@gmail.com>
2026-01-21 22:17:22 +0530

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './assets/fonts/fonts.css'
 import './critical.css'
 import './performance.css'
 import './index.css'
